1e5c67da1aa43e34d7a0d45e12e5cc333ba7df4b
PeachCube <nathanpeach500@live.com>
2023-11-08 14:27:39 -0400

    Changed the width and height to map.GetLength() and made the top and bottom borders adjust by scale

--- Text-Based RPG Map - Nathan Peach/Program.cs
+++ Text-Based RPG Map - Nathan Peach/Program.cs
@@ -23,8 +23,8 @@ namespace Text_Based_RPG_Map___Nathan_Peach
         {'`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
         {'`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`'},
     };
-        static int width = 30;
-        static int height = 12;
+        static int width = map.GetLength(1);
+        static int height = map.GetLength(0);
         // usage: map[y, x] <--- coordinates start at 0,0 and not 1,1; no negative coordinates. To find [3,4], you would first move down to the fourth tile, then move to the fifth right tile.
 
         // map legend:
@@ -35,33 +35,50 @@ namespace Text_Based_RPG_Map___Nathan_Peach
 
         static void Main(string[] args)
         {
-            DisplayMap();
+            //DisplayMap();
+            DisplayMap(2);
             Console.ReadKey(true);
         }
         static void DisplayMap()//Unscaled version
         {
-            Console.WriteLine("+------------------------------+");
+            Console.WriteLine("+------------------------------+");//Hard-coded for now because the map is unscaled
             for (int x = 0; x <= width - 1; x++)
             {
                 for (int y = 0; y <= height - 1; y++, Console.WriteLine("|"))
                 {
-
+                    Console.WriteLine("|");
                     Console.SetCursorPosition(x + 1, y + 1);
                     Console.Write(map[y, x]);
                 }
-                Console.WriteLine("+------------------------------+");
-            }
+            } 
+            Console.WriteLine("+------------------------------+");
         }
+        //The display of the map is offset by Y+1 and x+1 to make space for the border. The top and bottom borders are placed outside of the for loops, and the
+        //side borders are integrated into the loops. 
+        //
         static void DisplayMap(int scale)//Scaled version
         {
-            for (int x = 0; x <= width - 1; x++)
+            Console.Write("+");
+            for (int scaling = 0;scaling < scale; scaling ++)
             {
-                for (int y = 0; y <= height - 1; y++, Console.WriteLine("|"))
+                Console.Write("------------------------------");
+            }
+            Console.WriteLine("+");
+            for (int x = 0; x <= (width)- 1; x++)
+            {
+                for (int y = 0; y <= (height) - 1; y++, Console.WriteLine("|"))
                 {
-                    Console.SetCursorPosition(x + 1 + scale, y + 1 + scale);
+                    Console.WriteLine("|");
+                    Console.SetCursorPosition(x + 1, y + 1);
                     Console.Write(map[y, x]);
                 }
             }
+            Console.Write("+");
+            for (int scaling = 0; scaling < scale; scaling++)
+            {
+                Console.Write("------------------------------");
+            }
+            Console.WriteLine("+");
         }
     }
 }
